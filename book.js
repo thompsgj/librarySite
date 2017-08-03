c868d5ff950039476ce9079664eb4c5100c657d6
Greg Thompson <thompsgj@yahoo.com>
2017-08-03 15:27:39 +0900

    Slight fix

--- book.js
+++ book.js
@@ -3,7 +3,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser')
 
-require('./app_api/models/db');
+var monk = require('monk');
+var db = monk('localhost:27017/bookdb');
+require('./app_api/models/db.js');
 var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
 
