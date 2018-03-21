cd2835abfe0994257ca04584a0693d89ea4bf099
Greg Thompson <thompsgj@yahoo.com>
2018-03-21 22:32:50 +0900

    Upload

--- helloworld.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req,res) {
-	res.writeHead(200, { 'Content-Type' : 'text/plain' });
-	res.end('Hello');
-}).listen(3000);
-
-console.log('Server start on localhost:3000; press Ctrl-C to terminate...');
